48ddca84aaa4a1340ce48f98aea59f7e27f92676
fmahadyBD <fmahady01@gmail.com>
2025-05-31 16:36:03 +0600

    update the contorller

--- src/main/java/com/example/jenkins_test_1/Controllers.java
+++ src/main/java/com/example/jenkins_test_1/Controllers.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Controllers {
 
-    @GetMapping("/")
+    @GetMapping("/home")
     public String getPage(){
         return "home";
     }
