f9b48e27ed446335e00c382d218524ad894af168
fmahadyBD <fmahady01@gmail.com>
2025-05-31 16:47:10 +0600

    downgrede the  pom.xml

--- src/main/java/com/example/jenkins_test_1/Controllers.java
+++ src/main/java/com/example/jenkins_test_1/Controllers.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class Controllers {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String getPage(){
         return "home";
     }
